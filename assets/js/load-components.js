2d88cc615ffaef9e0ac12ba95d1bb44b04f991e4
AiNga04 <aingaspkt@gmail.com>
2024-12-24 12:28:52 +0700

    fix(url): fix url not working

--- assets/js/load-components.js
+++ assets/js/load-components.js
@@ -1,17 +1,17 @@
-// Hàm tải nội dung HTML từ file và chèn vào phần tử có ID tương ứng
-function loadHTML(file, elementId) {
-  fetch(file)
-    .then((response) => {
-      if (!response.ok) throw new Error(`Không thể tải file: ${file}`);
-      return response.text();
-    })
-    .then((data) => {
-      document.getElementById(elementId).innerHTML = data;
-    })
-    .catch((error) => console.error(error));
-}
+// // Hàm tải nội dung HTML từ file và chèn vào phần tử có ID tương ứng
+// function loadHTML(file, elementId) {
+//   fetch(file)
+//     .then((response) => {
+//       if (!response.ok) throw new Error(`Không thể tải file: ${file}`);
+//       return response.text();
+//     })
+//     .then((data) => {
+//       document.getElementById(elementId).innerHTML = data;
+//     })
+//     .catch((error) => console.error(error));
+// }
 
-// Tải header và footer
-loadHTML("_navbar.html", "navbar");
-loadHTML("_contact.html", "contact");
-loadHTML("_footer.html", "footer");
+// // Tải header và footer
+// loadHTML("./_navbar.html", "navbar");
+// loadHTML("./_contact.html", "contact");
+// loadHTML("./_footer.html", "footer");
